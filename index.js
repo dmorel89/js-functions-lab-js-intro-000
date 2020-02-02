4691c64964abee8c1ff3f43f71c33d1ba89abd47
Daniel Alejandro More Cespedes <dmorelwebdesign@gmail.com>
2020-02-02 02:17:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}
+happyHolidays()
+
+function happyHolidayTo(name){
+return `Happy holidays, ${name}!`
+}
+
+
+function happyHolidaysTo(name, holiday){
+return `Happy ${holiday}, ${name}!`
+}
+
+
+
+function holidayCountdown(days , holiday){
+return `It's ${days} days until ${holiday}!`
+}// write your code below!
